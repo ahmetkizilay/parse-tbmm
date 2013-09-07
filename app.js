546f111f440a79ecbf85ccdcf421fc7d3cf2019b
ahmet kizilay <ahmet.kizilay@gmail.com>
2013-09-07 19:55:19 +0300

    bismil

---
+++ app.js
@@ -0,0 +1,14 @@
+(function () {
+    /* jslint node: true */
+    'use strict';
+
+    var url = require('url'),
+        htmlParser = require('htmlparser'),
+        http = require('http'),
+        select = require('soupselect').select,
+        link = 'http://www.tbmm.gov.tr/develop/owa/milletvekillerimiz_sd.mv_liste?p_donem_kodu=24',
+        reqOptions = url.parse(link);
+
+        
+
+})();
